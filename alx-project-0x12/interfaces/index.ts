2e85e849ffb0603babf2ff9a4b0c3fd6a710710e
faithakinbobola <faithakinbobola2@gmail.com>
2025-08-10 02:02:28 +0100

    Generate Image based on user prompt

---
+++ alx-project-0x12/interfaces/index.ts
@@ -0,0 +1,20 @@
+import { StringifyOptions } from "querystring"
+import * as React from "react"
+
+export interface ReactComponentProps {
+    children: React.ReactNode
+}
+
+export interface GeneratedImagProps {
+    imageUrl: string
+    prompt: string
+    width?: string
+    height?: string
+    action: (imagePath: string) => void
+}
+
+export type RequestProps = {
+    prompt: string
+}
+
+export type ImageProps = Pick<GeneratedImagProps, "imageUrl" | "prompt">
